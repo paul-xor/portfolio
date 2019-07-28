22c24332e971d3e8fbeafe9a34a4a210a26868d0
Pasha Smolov <palexsmol@gmail.com>
2019-07-27 19:00:19 -0700

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,41 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
 project_list = [
-  [ "Germany", 81831000 ],
-  [ "France", 65447374 ],
-  [ "Belgium", 10839905 ],
-  [ "Netherlands", 16680000 ]
+[ "ALFA-BLOG", 
+"<p>DESCRIPTION: Blog site build on Ruby on Rails, includes:
+<li> CRUD</li>
+<li> User Authentification</li>
+<li> User Admin</li>
+<li> Gravatar</li>
+<li> Smart WYSIWYG - CKEditor</li></p>",
+"https://wanawaka.herokuapp.com",
+"https://github.com/paul-xor/blog_on_rails"
+],
+[ "STARTDB", 
+"<p>DESCRIPTION: API fetch build on React, includes:
+<li> Fetch data from swapi API</li>
+<li> Bootstrap 4, theme Sandstone</li></p>",
+"https://stardb.herokuapp.com/",
+"https://github.com/paul-xor/stardb"
+],
+[ "SOMMA", 
+"<p>DESCRIPTION: App allows authentication via Facebook and 
+post on faceboot page, includes:
+<li> Facebook API</li>
+<li> Bootstrap 4, theme Sketchy</li></p>",
+"https://somma-rails.herokuapp.com/",
+"https://github.com/paul-xor/somma-rails/tree/solution"
+],
+[ "CODEOVER", 
+"<p>DESCRIPTION: Quiz app - teamwork on CodeCore, includes:
+<li> Facebook API</li>
+<li> Bootstrap 4, theme Sketchy</li></p>",
+"https://somma-rails.herokuapp.com/",
+"https://github.com/paul-xor/somma-rails/tree/solution"
+]
 ]
 
 project_list.each do |name, description, site_link, git_link|
-  Project.create( name: name, description: description, site_link: site_link, site_link: site_link )
+  Project.create( name: name, description: description, site_link: site_link, git_link: git_link )
 end
