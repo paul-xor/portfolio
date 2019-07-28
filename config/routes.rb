713fd770eb73b71b2b58fb47b7a3334fd12f085d
Pasha Smolov <palexsmol@gmail.com>
2019-07-27 20:13:29 -0700

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :projects
   root 'pages#main'
+  devise_for :users
+  resources :projects
   get '/about', to: 'pages#about'
   get '/contacts', to: 'pages#contacts'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
